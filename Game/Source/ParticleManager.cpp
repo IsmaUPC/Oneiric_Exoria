59fdc80cd2056e132649b06f88c0b69f3b866c09
IsmaUPC <itejada.UPC@gmail.com>
2021-06-08 11:58:19 +0200

    Light dungeon

--- Game/Source/ParticleManager.cpp
+++ Game/Source/ParticleManager.cpp
@@ -1,130 +1,135 @@
-//#include "ParticleManager.h"
-//
-//#include "Defs.h"
-//#include "Log.h"
-//
-//ParticleManager::ParticleManager() : Module()
-//{
-//	name.Create("particle_manager");
-//}
-//
-//// Destructor
-//ParticleManager::~ParticleManager()
-//{}
-//
-//// Called before ParticleManager is available
-//bool ParticleManager::Awake(pugi::xml_node& config)
-//{
-//	bool ret = true;
-//
-//	return ret;
-//}
-//
-//// Called before the first frame
-//bool ParticleManager::Start()
-//{
-//	LOG("ParticleManager start");
-//	active = true;
-//
-//	// Load Fx
-//
-//	// Load Textures
-//	texParticles = app->tex->Load("Textures/");
-//
-//	// Animations
-//
-//
-//	return true;
-//}
-//
-//// Called each loop iteration
-//bool ParticleManager::PreUpdate()
-//{
-//	for (ListItem<Particle*>* particle = particles->start; particle; particle = particle->next)
-//		particle->data->PreUpdate();
-//
-//	return true;
-//}
-//
-//bool ParticleManager::Update(float dt)
-//{
-//	if (!app->sceneManager->GetIsPause())
-//	{
-//		SpeedAnimationCheck(dt);
-//
-//		for (ListItem<Particle*>* particle = particles->start; particle; particle = particle->next)
-//			particle->data->Update(dt);
-//	}
-//
-//	return true;
-//}
-//void ParticleManager::SpeedAnimationCheck(float dt)
-//{
-//	for (int i = 0; i < animations->Count(); i++)
-//	{
-//		animations->At(i)->data->speed = dt * 6;
-//	}
-//}
-//
-//bool ParticleManager::AddParticle(iPoint* pos, iPoint* vMax, int lifetime, TypeParticle type, bool isActive = true)
-//{
-//	Particle* particle;
-//	 
-//	if(particle = new Particle(pos,vMax,lifetime,type))	particles->Add(particle);
-//	else
-//	{
-//		LOG("Failure detected when creating particle: %c", SDL_GetError());
-//		return false;
-//	}
-//
-//	LOG("Successful particle creation");
-//	return true;
-//}
-//
-//bool ParticleManager::PostUpdate()
-//{
-//	if (!app->sceneManager->GetWinBattle())
-//	{
-//		
-//		for (ListItem<Particle*>* particle = particles->start; particle; particle = particle->next)
-//			particle->data->PostUpdate();
-//	}
-//
-//	return true;
-//}
-//
-//// Called before quitting
-//bool ParticleManager::CleanUp()
-//{
-//	LOG("Destroying ParticleManager");
-//
-//	bool ret = true;
-//	ClearList(ret);
-//
-//	// Unload Tx
-//	app->tex->UnLoad(texParticles);
-//
-//	// Unload Animations
-//	animations->Clear();
-//	//RELEASE(isDetectedAnim);
-//
-//	active = false;
-//
-//	return ret;
-//}
-//
-//void ParticleManager::ClearList(bool& ret)
-//{
-//
-//	// Clear list
-//	particles->Clear();
-//	spawnQueue->Clear();
-//}
-//
-//bool ParticleManager::LoadState(pugi::xml_node&)
-//{
-//	return false;
-//}bool ParticleManager::LoadParticleProperties()
+#include "ParticleManager.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+ParticleManager::ParticleManager() : Module()
+{
+	name.Create("particle_manager");
+}
+
+// Destructor
+ParticleManager::~ParticleManager()
+{}
+
+// Called before ParticleManager is available
+bool ParticleManager::Awake(pugi::xml_node& config)
+{
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool ParticleManager::Start()
+{
+	LOG("ParticleManager start");
+	active = true;
+
+	// Load Fx
+
+	// Load Textures
+	texParticles = app->tex->Load("Textures/");
+
+	// Animations
+
+
+	return true;
+}
+
+// Called each loop iteration
+bool ParticleManager::PreUpdate()
+{
+	for (ListItem<Particle*>* particle = particles->start; particle; particle = particle->next)
+		particle->data->PreUpdate();
+
+	return true;
+}
+
+bool ParticleManager::Update(float dt)
+{
+	if (!app->sceneManager->GetIsPause())
+	{
+		SpeedAnimationCheck(dt);
+
+		for (ListItem<Particle*>* particle = particles->start; particle; particle = particle->next)
+			particle->data->Update(dt);
+	}
+
+	return true;
+}
+void ParticleManager::SpeedAnimationCheck(float dt)
+{
+	for (int i = 0; i < animations->Count(); i++)
+	{
+		animations->At(i)->data->speed = dt * 6;
+	}
+}
+
+bool ParticleManager::AddParticle(iPoint* pos, iPoint* vMax, int lifetime, TypeParticle type, bool isActive)
+{
+	Particle* particle;
+	 
+	if(particle = new Particle(pos,vMax,lifetime,type))	particles->Add(particle);
+	else
+	{
+		LOG("Failure detected when creating particle: %c", SDL_GetError());
+		return false;
+	}
+
+	LOG("Successful particle creation");
+	return true;
+}
+
+bool ParticleManager::PostUpdate()
+{
+	if (!app->sceneManager->GetWinBattle())
+	{
+		
+		for (ListItem<Particle*>* particle = particles->start; particle; particle = particle->next)
+			particle->data->PostUpdate();
+	}
+
+	return true;
+}
+
+// Called before quitting
+bool ParticleManager::CleanUp()
+{
+	LOG("Destroying ParticleManager");
+
+	bool ret = true;
+	ClearList(ret);
+
+	// Unload Tx
+	app->tex->UnLoad(texParticles);
+
+	// Unload Animations
+	animations->Clear();
+	//RELEASE(isDetectedAnim);
+
+	active = false;
+
+	return ret;
+}
+
+void ParticleManager::ClearList(bool& ret)
+{
+
+	// Clear list
+	particles->Clear();
+	spawnQueue->Clear();
+}
+
+bool ParticleManager::LoadState(pugi::xml_node&)
+{
+	return false;
+}
+bool ParticleManager::SaveState(pugi::xml_node&) const
+{
+	return false;
+}
+//bool ParticleManager::LoadParticleProperties()
 //{
 //	bool ret = false;
 //	int numTypes = (int)TypeParticle::COUNT;
@@ -139,6 +144,8 @@
 //	}
 //	else
 //	{
+//
+//
 //		rootNode = propertiesFile.first_child();
 //		ParticleProperties* propertyOfType = new ParticleProperties();
 //
@@ -170,10 +177,5 @@
 //
 //	}
 //
-//	return false;
-//}
-//
-//bool ParticleManager::SaveState(pugi::xml_node&) const
-//{
 //	return false;
 //}
